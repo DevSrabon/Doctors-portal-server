6c914b660af8cc186cf46c38383d0ecc78dac657
devsrabon <et.srabon@gmail.com>
2022-12-03 12:01:16 +0600

    send email

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+const nodemailer = require('nodemailer');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
+const mg = require("nodemailer-mailgun-transport");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -19,6 +21,39 @@ const client = new MongoClient(uri, {
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
+
+function sendBookingEmail(booking) {
+	const { email, treatment, appointmentDate, slot } = booking;
+		const auth = {
+			auth: {
+				api_key: process.env.EMAIL_SEND_KEY,
+				domain: process.env.EMAIL_SEND_DOMAIN,
+			},
+		};
+	const transporter = nodemailer.createTransport(mg(auth));
+	transporter.sendMail(
+		{
+			from: "srabonfb@gmail.com", // verified sender email
+			to: email || "srabonfb@gmail.com", // recipient email
+			subject: `Your appointment for ${treatment} is confirmed`, // Subject line
+			text: "Hello world!", // plain text body
+			html: `
+			<h3> Your appointment is confirmed</h3>
+			<div>
+			<p>Your appointment for treatment: ${treatment}</p>
+			<p>Please visit us on : ${appointmentDate} at ${slot}</p>
+			</div>
+			`, // html body
+		},
+		function (error, info) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log("Email sent: " + info.response);
+			}
+		}
+	);
+		}
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -50,6 +85,7 @@ async function run() {
 			.collection("usersCollection");
 		const doctorsCollection = client.db("doctorsPortal").collection("doctors");
 		const paymentsCollection = client.db("doctorsPortal").collection("payments");
+
 
 		//NOTE: make sure you use verifyAdmin after verifyJWT
 		const verifyAdmin = async (req, res, next) => {
@@ -153,7 +189,6 @@ async function run() {
 				return res.status(403).send({ message: "forbidden access" });
 			}
 			const query = { email: email };
-
 			const bookings = await bookingCollection.find(query).toArray();
 			res.send(bookings);
         });
@@ -178,6 +213,9 @@ async function run() {
 				return res.send({ acknowledged: false, message });
 			}
 			const result = await bookingCollection.insertOne(booking);
+			// send email about appointment confirmation
+			sendBookingEmail(booking)
+
 			res.send(result);
         });
         
@@ -257,17 +295,17 @@ async function run() {
 		});
 
 		// temporary to update price field on appointment options
-		app.get("/addPrice", async (req, res) => {
-            const filter = {};
-            const options = { upsert: true };
-            const updateDoc = {
-							$set: {
-								price: "99",
-							},
-            };
-            const result = await appointmentCollection.updateMany(filter, updateDoc, options);
-            res.send(result)
-		});
+		// app.get("/addPrice", async (req, res) => {
+        //     const filter = {};
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+		// 					$set: {
+		// 						price: "99",
+		// 					},
+        //     };
+        //     const result = await appointmentCollection.updateMany(filter, updateDoc, options);
+        //     res.send(result)
+		// });
 
 		app.post("/users", async (req, res) => {
 			const user = req.body;
